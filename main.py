b55af5d1b14c09104622355caf847cce5f5ae9a4
kenwaytis <kenwaytis@163.com>
2023-08-29 17:40:49 +0800

    init

--- main.py
+++ main.py
@@ -11,3 +11,9 @@ model = WhisperModel(model_size, device="cuda", compute_type="float16")
 
 app = FastAPI()
 
+class Audio(BaseModel):
+    audio: str
+
+async def b64_decode(b64_file):
+    file_data = base64.b64decode(b64_file)
+    return file_data
